4a92328909145f767cf0036120f8d1b7d9816177
William Montenegro <williammontenegro4d@gmail.com>
2025-04-03 00:12:10 -0500

    Pulir k6-config

--- k6-config.js
+++ k6-config.js
@@ -1,44 +1,31 @@
 export const config = {
-  baseURL: "https://quickpizza.grafana.com",
-  timeout: 5000,
+  baseURL: __ENV.BASE_URL || "https://quickpizza.grafana.com",
+  timeout: __ENV.TIMEOUT || 5000,
   stages: {
     load: [
-      { duration: "1m", target: 50 },
-      { duration: "2m", target: 100 },
+      { duration: "2m", target: 50 },
+      { duration: "3m", target: 100 },
       { duration: "1m", target: 0 },
     ],
     stress: [
       { duration: "5m", target: 500 },
-      { duration: "2m", target: 1000 },
+      { duration: "3m", target: 1000 },
       { duration: "2m", target: 0 },
     ],
     soak: [
-      { duration: "1h", target: 200 },
-      { duration: "2h", target: 200 },
-      { duration: "1h", target: 0 },
-    ],
-    spike: [
-      { duration: "10s", target: 100 },
-      { duration: "1m", target: 1000 },
-      { duration: "10s", target: 0 },
-    ],
-    endurance: [
-      { duration: "30m", target: 300 },
-      { duration: "1h", target: 300 },
+      { duration: "30m", target: 200 },
+      { duration: "30m", target: 200 },
       { duration: "30m", target: 0 },
     ],
-    ramp: [
-      { duration: "5m", target: 50 },
-      { duration: "10m", target: 200 },
-      { duration: "5m", target: 50 },
-      { duration: "5m", target: 0 },
+    spike: [
+      { duration: "5s", target: 500 },
+      { duration: "20s", target: 2000 },
+      { duration: "10s", target: 0 },
     ],
-    smoke: [
-      { duration: '10s', target: 5 }
-    ]
+    smoke: [{ duration: "5s", target: 5 }],
   },
   cloud: {
-    projectID: __ENV.PROJECT_ID,
-    name: __ENV.TEST_NAME,
-  }
+    projectID: __ENV.PROJECT_ID || "default_project",
+    name: __ENV.TEST_NAME || "default_test",
+  },
 };
